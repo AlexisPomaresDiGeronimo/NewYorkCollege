d88b316d9357027857a638d4af7eaa637b368540
AlexisPomaresDiGeronimo <alexispomares24@hotmail.com>
2022-11-02 18:42:40 +0200

    Spring Initializr

---
+++ MessagingWebSocket/src/main/java/com/alexis/MessagingWebSocket/MessagingWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.alexis.MessagingWebSocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingWebSocketApplication.class, args);
+	}
+
+}
